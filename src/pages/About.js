5cdf6cdf62a3780b6c014ad4fd4f0c66b82ac2a1
Saveliy <savisafe@vk.com>
2022-03-08 01:15:10 +0700

    all

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">
+                Это приложение написано Savisafe
+            </h1>
+            <p className="lead">
+                версия приложения <strong>1.0</strong>
+            </p>
+        </div>
+    </div>
+)
